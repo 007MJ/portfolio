91095b6847360e5b7b1567931f214d119bcbd23a
007MJ <jetforce@jetforces-mbp.home>
2024-10-13 11:52:36 -0400

    done

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 function displaySidebar(){
-    const header_responsive = document.querySelector(".sidebar");
-  header_responsive.style.display = "flex";
+    const header_responsive = document.querySelector(".sidebar a");
+    console.log(header_responsive)
+    header_responsive.style.display = "flex";
+    console.log(header_responsive)
 }
 
