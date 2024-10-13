eb9b34ecc184597daee57f101f8fdf0ae3115d32
007MJ <k.mijiza@gmail.com>
2024-10-13 15:45:31 -0400

    responsive bar nice

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 
 function displaySidebar(){
-    const header_responsive = document.querySelector(".sidebar a");
-    console.log(header_responsive)
+    const header_responsive = document.querySelector(".links");
     header_responsive.style.display = "flex";
-    console.log(header_responsive)
+    const icone_open = document.querySelector(".icon_nav");
+    const bg_header =  document.querySelector(".header");
+    bg_header.style.backgroundColor = "#97CDF2"
+    icone_open.style.display = "none";
+    const close_nav =  document.querySelector(".close_nav");
+    close_nav.style.display = "flex"
 }
 
+function  close_navbar(){
+    const close_nav =  document.querySelector(".close_nav");
+    const header_responsive = document.querySelector(".links");
+    header_responsive.style.display = "none";
+    close_nav.style.display = "none"
+    const icone_open = document.querySelector(".icon_nav");
+    icone_open.style.display = "flex";
+
+}
