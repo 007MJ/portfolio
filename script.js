60ddbbc75fbe0fb5dfcb645f770b791c8252330e
007MJ <k.mijiza@gmail.com>
2024-10-14 15:26:08 -0400

    nice work

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ function  close_navbar(){
     close_nav.style.display = "none";
     open_nav.style.display = "flex";
 }
+
+
+let slider =  document.querySelector(".main-skill");
+let innerSlider = document.querySelector(".section-skill");
+
+
+let pressed = false;
+let startx;
+let x;
+console.log(slider);
+slider.addEventListener("click", (event) =>{
+    pressed = true;
+    startx = e.offsetX;
+    console.log(startx);
+    console.log(2);
+});
+console.log(3);
