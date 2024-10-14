cddb964ebcd65eda7010aaa76f793bef0c0d80fc
007MJ <k.mijiza@gmail.com>
2024-10-14 12:55:55 -0400

    responsive bar almost done

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
 
 function displaySidebar(){
-    const header_responsive = document.querySelector(".links");
-    header_responsive.style.display = "flex";
-    const icone_open = document.querySelector(".icon_nav");
-    const bg_header =  document.querySelector(".header");
-    bg_header.style.backgroundColor = "#97CDF2"
-    icone_open.style.display = "none";
-    const close_nav =  document.querySelector(".close_nav");
-    close_nav.style.display = "flex"
+    const close_nav =  document.querySelector(".header-reponsive");
+    const open_nav =  document.querySelector(".header");
+    close_nav.style.display = "flex";
+    open_nav.style.display = "none";
 }
 
 function  close_navbar(){
-    const close_nav =  document.querySelector(".close_nav");
-    const header_responsive = document.querySelector(".links");
-    header_responsive.style.display = "none";
-    close_nav.style.display = "none"
-    const icone_open = document.querySelector(".icon_nav");
-    icone_open.style.display = "flex";
-    const bg_header =  document.querySelector(".header");
-    bg_header.style.backgroundColor = ""
-
+    const close_nav =  document.querySelector(".header-reponsive");
+    const open_nav =  document.querySelector(".header");
+    close_nav.style.display = "none";
+    open_nav.style.display = "flex";
 }
